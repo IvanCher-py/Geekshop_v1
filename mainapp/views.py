803c035a2b4fb1c91d6a7ff962f6ef657c6815a9
Ivan <cherepov.iv@yandex.ru>
2020-12-13 11:39:24 +0300

    Lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from mainapp.models import ProductCategory, Product
 
 def main(request):
     content = {
@@ -11,19 +11,22 @@ def main(request):
 def products(request):
     content = {
         'title': 'SupShop - Категории',
-        'products': [
-            {'foto': '/static/vendor/img/products/Adidas-hoodie.png', 'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00 руб.', 'info': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
-            {'foto': '/static/vendor/img/products/Blue-jacket-The-North-Face.png', 'name': 'Синяя куртка The North Face', 'price': '23 725,00 руб.', 'info': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
-            {'foto': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png', 'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00 руб.', 'info': 'Материал с плюшевой текстурой. Удобный и мягкий.'},
-            {'foto': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png', 'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00 руб.', 'info': 'Плотная ткань. Легкий материал.'},
-            {'foto': '/static/vendor/img/products/Black-Dr-Martens-shoes.png', 'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13 590,00 руб.', 'info': 'Гладкий кожаный верх. Натуральный материал.'},
-            {'foto': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png', 'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2 890,00 руб.', 'info': 'Легкая эластичная ткань сирсакер Фактурная ткань.'},
-        ]
+        'products': Product.objects.all()
     }
     return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+def clothes(request):
+    contex = {
+        'title': 'SupShop - Категории',
+        'products': Product.objects.all()
+    }
+    return render(request, 'mainapp/clothes.html', context=contex)
+
+
 
 # Create your views here.
